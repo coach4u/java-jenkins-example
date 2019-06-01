c4218ad1b72ee8deed7670cfc42d69417c374f73
root <root@ip-172-31-3-199.ap-south-1.compute.internal>
2019-06-01 06:12:40 +0000

    to add

---
+++ coachrhca-app/src/main/java/com/coachrhca/app/App.java
@@ -0,0 +1,13 @@
+package com.coachrhca.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World From Coach rhca, devops" );
+    }
+}
